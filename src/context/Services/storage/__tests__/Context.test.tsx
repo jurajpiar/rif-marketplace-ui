555019221e5eedb9901bce7798638adc79bdc72b
Juraj Piar <juraj.piar@icloud.com>
2021-02-15 13:26:14 +0100

    feat(tests-unit): adds storage offfers tests

--- src/context/Services/storage/__tests__/Context.test.tsx
+++ src/context/Services/storage/__tests__/Context.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { render } from '@testing-library/react'
 import { useProviderTest } from '__tests__/testUtils'
 
